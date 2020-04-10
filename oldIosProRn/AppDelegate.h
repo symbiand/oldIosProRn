df3ec926d9fc7173dd734c17468685fb0de8481d
fang1 <gfan@192.168.36.57>
2020-04-10 21:55:00 +0800

    first commit

---
+++ oldIosProRn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  oldIosProRn
+//
+//  Created by gfan on 2020/4/8.
+//  Copyright © 2020 uni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ oldIosProRn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oldIosProRn
+//
+//  Created by gfan on 2020/4/8.
+//  Copyright © 2020 uni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
